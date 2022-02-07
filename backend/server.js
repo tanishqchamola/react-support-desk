ef8dac4d053713a3bb7e0bb9e5ccb8fee824e78f
Tanishq Chamola <chamola.tanishq@gmail.com>
2022-02-08 01:43:54 +0530

    Added basic routes, jwt, gaurd

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("server");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 8000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+	res.json("hi");
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}...`));
